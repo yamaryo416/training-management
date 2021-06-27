995ec746365ca96b5107994494272fd7b47590d6
Ryo Yamaguchi <rys0707@gmail.com>
2021-06-27 14:13:21 +0900

    next teams/[id].tsxを編集: errorOneTeamFromId時とerrorMyProfile時にページ遷移するよう追加

--- frontend/src/pages/teams/[id].tsx
+++ frontend/src/pages/teams/[id].tsx
@@ -1,4 +1,4 @@
-import { memo, VFC } from "react";
+import { memo, VFC, useEffect } from "react";
 import { useRouter } from 'next/router';
 import { useQuery } from "@apollo/client";
 
@@ -6,16 +6,37 @@ import { OneTeamFromIdType } from "../../../types/queriesType";
 import { GET_ONE_TEAM_FROM_ID } from "../../queries";
 import { CustomSpinner } from "../../components/atoms/spinner/CustomSpinner"
 import { HeadTitle } from '../../components/atoms/title/HeadTitle'
+import { useGetMyProfile } from "../../hooks/queries/useGetMyProfile";
+import { useMessage } from "../../hooks/useMessage";
+import { FailedText } from "../../components/atoms/text/FailedText";
 
 const TeamDetail: VFC = memo(() => {
     const router = useRouter()
     const { id } = router.query
 
+    const { showMessage } = useMessage()
+
+    const { errorMyProfile ,loadingMyProfile, dataMyProfile } = useGetMyProfile()
     const {loading: loadingOneTeamFromId, data: dataOneTeamFromId, error: errorOneTeamFromId } = useQuery<OneTeamFromIdType>(GET_ONE_TEAM_FROM_ID, {
         variables: { teamId: id }
     })
 
-    if (loadingOneTeamFromId) return <CustomSpinner />
+    useEffect(() => {
+        if (errorMyProfile) {
+            localStorage.removeItem('token')
+        }
+        if (!localStorage.getItem('token')) {
+            router.push('/')
+            showMessage({ title: 'ログインしてください。', status: 'error'})
+        }
+        if (errorOneTeamFromId) {
+            router.push('/teams')
+        }
+    }, [])
+    
+
+    if (loadingMyProfile || loadingOneTeamFromId) return <CustomSpinner />
+    else if (errorMyProfile || errorOneTeamFromId) return <FailedText />
 
     return (
         <>
