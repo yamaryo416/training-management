dec96c149471e4c5c79d4f7a504170872426d627
Ryo Yamaguchi <rys0707@gmail.com>
2021-06-27 14:07:40 +0900

    next teams/[id].tsxを編集: ヘッダータイトルを追加

--- frontend/src/pages/teams/[id].tsx
+++ frontend/src/pages/teams/[id].tsx
@@ -1,13 +1,25 @@
 import { memo, VFC } from "react";
 import { useRouter } from 'next/router';
+import { useQuery } from "@apollo/client";
+
+import { OneTeamFromIdType } from "../../../types/queriesType";
+import { GET_ONE_TEAM_FROM_ID } from "../../queries";
+import { CustomSpinner } from "../../components/atoms/spinner/CustomSpinner"
+import { HeadTitle } from '../../components/atoms/title/HeadTitle'
 
 const TeamDetail: VFC = memo(() => {
     const router = useRouter()
     const { id } = router.query
 
+    const {loading: loadingOneTeamFromId, data: dataOneTeamFromId, error: errorOneTeamFromId } = useQuery<OneTeamFromIdType>(GET_ONE_TEAM_FROM_ID, {
+        variables: { teamId: id }
+    })
+
+    if (loadingOneTeamFromId) return <CustomSpinner />
+
     return (
         <>
-            TeamDetail
+            <HeadTitle title={dataOneTeamFromId?.oneTeamFromId.name} />
         </>
     )
 })
