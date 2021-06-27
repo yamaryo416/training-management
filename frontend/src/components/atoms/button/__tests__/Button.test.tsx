6aabbfdd3f4d8ae14f933b06fd8a58bbbeed37be
Ryo Yamaguchi <rys0707@gmail.com>
2021-06-27 18:00:43 +0900

    next Button.test.tsxを編集: PrimaryLargeButtonとDeleteButtonのテストを追加

--- frontend/src/components/atoms/button/__tests__/Button.test.tsx
+++ frontend/src/components/atoms/button/__tests__/Button.test.tsx
@@ -4,7 +4,9 @@ import { render, screen, cleanup } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import { PrimaryButton } from '../PrimaryButton'
+import { PrimaryLargeButton } from '../PrimaryLargeButton'
 import { SecondaryButton } from '../SecondaryButton'
+import { DeleteButton } from '../DeleteButton'
 
 afterEach(cleanup)
 
@@ -43,6 +45,25 @@ describe('PrimaryButton', () => {
     })
 })
 
+describe('PrimaryLargeButton', () => {
+    const handleClick = jest.fn()
+
+    it('Should render correct text and Should calle handle click as clicked', () => {
+        render(
+            <PrimaryLargeButton
+                name='test'
+                onClick={() => handleClick()}
+            >
+                first large
+            </PrimaryLargeButton>
+        )
+        expect(screen.getByTestId('test-button')).toBeInTheDocument()
+        expect(screen.getByText('first large')).toBeInTheDocument()
+        userEvent.click(screen.getByText('first large'))
+        expect(handleClick).toBeCalled()
+    })
+})
+
 describe('SecondaryButton', () => {
     const handleClick = jest.fn() 
 
@@ -57,5 +78,41 @@ describe('SecondaryButton', () => {
         expect(screen.getByText('second')).toBeInTheDocument()
         userEvent.click(screen.getByText('second'))
         expect(handleClick).toBeCalled()
+    })
+})
+
+
+describe('DeleteButton', () => {
+    const handleClick = jest.fn() 
+
+    it('Should render correct text and Should calle handle click as clicked', () => {
+        render(
+            <DeleteButton
+                name='test'
+                type='button'
+                disabled={false}
+                onClick={() => handleClick()}
+            >
+                delete
+            </DeleteButton>
+        )
+        expect(screen.queryByTestId('test-button')).toBeInTheDocument()
+        expect(screen.getByText('delete')).toBeInTheDocument()
+        userEvent.click(screen.getByText('delete'))
+        expect(handleClick).toBeCalled()
+    })
+
+    it('Should have disabled attribute', () => {
+        render(
+            <DeleteButton
+                name='test'
+                type='button'
+                disabled={true}
+                onClick={() => handleClick()}
+            >
+                delete
+            </DeleteButton>
+        )
+        expect(screen.getByTestId('test-button')).toHaveAttribute('disabled')
     })
 })
