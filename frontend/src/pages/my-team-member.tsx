f9c33e0cbd5bebdc7507d48222aa47be3c5a97fb
Ryo Yamaguchi <rys0707@gmail.com>
2021-06-27 14:59:59 +0900

    next my-team-member.tsxを編集: errorMyProfile時とコーチ出ない場合、ページ遷移するように追加

--- frontend/src/pages/my-team-member.tsx
+++ frontend/src/pages/my-team-member.tsx
@@ -1,9 +1,35 @@
-import { memo, VFC } from "react";
+import { memo, VFC, useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { useGetMyProfile } from "../hooks/queries/useGetMyProfile";
+import { useMessage } from "../hooks/useMessage";
+import { CustomSpinner } from "../components/atoms/spinner/CustomSpinner";
+import { HeadTitle } from '../components/atoms/title/HeadTitle'
 
 const MyTeamMember: VFC = memo(() => {
+    const router = useRouter()
+    const { showMessage } = useMessage()
+    const { loadingMyProfile, dataMyProfile, errorMyProfile } = useGetMyProfile()
+
+    useEffect(() => {
+        if (errorMyProfile) {
+            localStorage.removeItem('token')
+        }
+        if (!localStorage.getItem('token')) {
+            router.push('/')
+            showMessage({ title: 'ログインしてください。', status: 'error'})
+        }
+        if (!dataMyProfile.myProfile.isCoach) {
+            router.push('/main')
+            showMessage({ title: '権限がありません。', status: 'error'})
+        }
+    }, [dataMyProfile])
+
+    if (loadingMyProfile) return <CustomSpinner />
+
     return (
         <>
-            マイチームメンバー
+            <HeadTitle title='マイチームメンバー' />
         </>
     )
 })
