6bf0d3a21511fced5cac8ac680c55b7991f20e46
Ryo Yamaguchi <rys0707@gmail.com>
2021-06-28 16:28:00 +0900

    next mockPostDataを作成, TeamBoardPostのテストを作成

---
+++ frontend/src/components/organisms/team/__tests__/TeamBoardPost.test.tsx
@@ -0,0 +1,157 @@
+import '@testing-library/jest-dom/extend-expect'
+
+import moment from "moment";
+import { render, screen, cleanup } from '@testing-library/react'
+import { RecoilRoot } from 'recoil';
+import { MockedProvider } from '@apollo/client/testing' 
+import userEvent from '@testing-library/user-event'
+
+import { mockMyProfileQuery } from '../../../../mocks/mockProfileData';
+import { mockFirstMyTeamPostsQuery, mockSecondMyTeamPostsQuery, mockErrorMyTeamPostsQuery, mockAbsentMyTeamPostsQuery } from '../../../../mocks/mockPostData';
+import { TeamBoardPost } from '../TeamBoardPost';
+
+const mocksCoach = [
+    mockMyProfileQuery(true, false),
+    mockFirstMyTeamPostsQuery,
+    mockSecondMyTeamPostsQuery,
+    mockFirstMyTeamPostsQuery
+]
+
+const mocksGeneral = [
+    mockMyProfileQuery(false, false),
+    mockFirstMyTeamPostsQuery
+]
+
+const mocksGuest = [
+    mockMyProfileQuery(false, true),
+    mockFirstMyTeamPostsQuery
+]
+
+const mocksErrorPostData = [
+    mockMyProfileQuery(true, false),
+    mockErrorMyTeamPostsQuery
+]
+
+const mocksAbsentPostData = [
+    mockMyProfileQuery(true, false),
+    mockAbsentMyTeamPostsQuery
+]
+
+beforeEach(() => {
+    jest.setTimeout(30000)
+})
+
+afterEach(cleanup)
+
+describe('TeamBoardPostItem', () => {
+    it('Should render without error', () => {
+        render(
+            <MockedProvider mocks={mocksCoach} addTypename={false}>
+                <RecoilRoot>
+                    <TeamBoardPost />
+                </RecoilRoot>
+            </MockedProvider>
+        )
+        expect(screen.queryByText('Loading...')).toBeInTheDocument()
+    })
+
+    it('Should render error message', async () => {
+        render(
+            <MockedProvider mocks={mocksErrorPostData} addTypename={false}>
+                <RecoilRoot>
+                    <TeamBoardPost />
+                </RecoilRoot>
+            </MockedProvider>
+        )
+        expect(await screen.findByText('データの取得に失敗しました。')).toBeInTheDocument()
+    })
+
+    it('Should render correct contents', async () => {
+        render(
+            <MockedProvider mocks={mocksCoach} addTypename={false}>
+                <RecoilRoot>
+                    <TeamBoardPost />
+                </RecoilRoot>
+            </MockedProvider>
+        )
+        expect(await screen.findByText("投稿一覧")).toBeInTheDocument()
+        expect(screen.queryAllByTestId('my-post').length).toEqual(5)
+        expect(screen.queryByText('post1')).toBeInTheDocument()
+        expect(screen.queryByTestId('1-post-created-at')).toHaveTextContent(moment().add(-1, 'days').format("M月D日 H時m分"))
+        expect(screen.queryAllByTestId('other-post').length).toEqual(5)
+        expect(screen.queryByText('post6')).toBeInTheDocument()
+        expect(screen.queryByTestId('6-post-created-at')).toHaveTextContent(moment().add(-6, 'days').format("M月D日 H時m分"))
+        expect(screen.queryByText("以前の10件の投稿")).toBeInTheDocument()
+        expect(screen.queryByText("閉じる")).not.toBeInTheDocument()
+    })
+
+    it('Should fetch more post after fetch link', async　() => {
+        render(
+            <MockedProvider mocks={mocksCoach} addTypename={false}>
+                <RecoilRoot>
+                    <TeamBoardPost />
+                </RecoilRoot>
+            </MockedProvider>
+        )
+        userEvent.click(await screen.findByText('以前の10件の投稿'))
+        expect(await screen.findByText('post11')).toBeInTheDocument()
+        expect(screen.queryAllByTestId('my-post').length).toEqual(7)
+        expect(screen.queryByText("閉じる")).toBeInTheDocument()
+        expect(screen.queryByText("以前の10件の投稿")).not.toBeInTheDocument()
+    })
+
+    it('Should render absent message', async () => {
+        render(
+            <MockedProvider mocks={mocksAbsentPostData} addTypename={false}>
+                <RecoilRoot>
+                    <TeamBoardPost />
+                </RecoilRoot>
+            </MockedProvider>
+        )
+        expect(await screen.findByText('投稿はありません。')).toBeInTheDocument()
+    })
+
+    describe('As Coach', () => {
+        it('Should render correct contents', async () => {
+            render(
+                <MockedProvider mocks={mocksCoach} addTypename={false}>
+                    <RecoilRoot>
+                        <TeamBoardPost />
+                    </RecoilRoot>
+                </MockedProvider>
+            )
+            expect(await screen.findByTestId('create-post-button')).toHaveTextContent('投稿')
+            expect(await screen.findByTestId('1-post-delete-icon')).toBeInTheDocument()
+            expect(screen.queryByTestId('6-post-delete-icon')).toBeInTheDocument()
+        })
+    })
+
+    describe('As General', () => {
+        it('Should render correct contents', async() => {
+            render(
+                <MockedProvider mocks={mocksGeneral} addTypename={false}>
+                    <RecoilRoot>
+                        <TeamBoardPost />
+                    </RecoilRoot>
+                </MockedProvider>
+            )
+            expect(await screen.findByTestId('create-post-button')).toHaveTextContent('投稿')
+            expect(await screen.findByTestId('1-post-delete-icon')).toBeInTheDocument()
+            expect(screen.queryByTestId('6-post-delete-icon')).not.toBeInTheDocument()
+        })
+    })
+
+    describe('As Guest', () => {
+        it('Should render correct contents', async() => {
+            render(
+                <MockedProvider mocks={mocksGuest} addTypename={false}>
+                    <RecoilRoot>
+                        <TeamBoardPost />
+                    </RecoilRoot>
+                </MockedProvider>
+            )
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            expect(screen.queryByTestId('create-post-button')).not.toBeInTheDocument()
+        })
+    })
+})
---
+++ frontend/src/mocks/mockPostData.ts
@@ -0,0 +1,112 @@
+import moment from "moment"
+import { graphql } from "msw"
+import { CREATE_POST, DELETE_POST, GET_MORE_MY_TEAM_POSTS } from "../queries"
+
+const mockMyPost = (id: number) => {
+    return {
+        id: String(id),
+        text: `post${id}`,
+        profile: {
+            id: '1',
+            nickname: 'coach user'
+        },
+        createdAt: moment().add(-id, 'days').toISOString()
+    }
+}
+
+const mockOtherPost = (id: number) => {
+    return {
+        id: String(id),
+        text: `post${id}`,
+        profile: {
+            id: '2',
+            nickname: 'general user'
+        },
+        createdAt: moment().add(-id, 'days').toISOString()
+    }
+}
+
+export const mockFirstMyTeamPostsQuery = { 
+    request: {
+        query: GET_MORE_MY_TEAM_POSTS,
+        variables: {
+            first: 10
+        }
+    },
+    result: {
+        data: {
+            myTeamPosts: {
+                edges: [
+                    { node: mockMyPost(1) },
+                    { node: mockMyPost(3) },
+                    { node: mockMyPost(3) },
+                    { node: mockMyPost(4) },
+                    { node: mockMyPost(5) },
+                    { node: mockOtherPost(6) },
+                    { node: mockOtherPost(7) },
+                    { node: mockOtherPost(8) },
+                    { node: mockOtherPost(9) },
+                    { node: mockOtherPost(10) },
+                ],
+                pageInfo: {
+                    endCursor: '10',
+                    hasNextPage: true
+                }
+            }
+        }
+    }
+}
+
+
+export const mockSecondMyTeamPostsQuery = {
+    request: {
+        query: GET_MORE_MY_TEAM_POSTS,
+        variables: {
+            first: 10, after: '10'
+        }
+    },
+    result: {
+        data: {
+            myTeamPosts: {
+                edges: [
+                    { node: mockMyPost(11) },
+                    { node: mockMyPost(12) }
+                ],
+                pageInfo: {
+                    endCursor: '12',
+                    hasNextPage: false
+                }
+            }
+        }
+    }
+}
+
+export const mockAbsentMyTeamPostsQuery = {
+    request: {
+        query: GET_MORE_MY_TEAM_POSTS,
+        variables: {
+            first: 10
+        }
+    },
+    result: {
+        data: {
+            myTeamPosts: {
+                edges: [],
+                pageInfo: {
+                    endCursor: '12',
+                    hasNextPage: false
+                }
+            }
+        }
+    }
+}
+
+export const mockErrorMyTeamPostsQuery = {
+    request: {
+        query: GET_MORE_MY_TEAM_POSTS,
+        variables: {
+            first: 10
+        }
+    },
+    error: new Error()
+}
