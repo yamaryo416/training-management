db0408ed8e854e8b0ce0e76a7f3cf54fb7dfc933
yamaryo416 <rys0707@gmail.com>
2021-06-30 16:21:53 +0900

    django prod_settingsを作成: secret_keyを切り分け, docker-composeを修正: 実行時にtest_settingを使うように修正

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,27 +11,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
-
 
 # Application definition
 
--- app/app/test_settings.py
+++ app/app/test_settings.py
@@ -1,3 +1,5 @@
 from .settings import *
 
+DEBUG = True
+
 SECRET_KEY = 'd8f)fvwh(ja7t!yx&bmo5-=u10s8pj_l5(f40*fgsmv4xuf_qy'
